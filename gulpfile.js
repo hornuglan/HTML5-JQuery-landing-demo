393883846795ec6585e59a3d3d07fb8e4947038f
hornuglan <hornuglan@gmail.com>
2018-10-11 01:00:24 +0300

    scripts in package.json were modified, gulpfile.js was fully modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
-"use strict";
-// Include gulp
 var gulp = require('gulp');
-
-// Include Our Plugins
-var sass = require('gulp-sass');
+var scssPlugin = require('gulp-sass');
 var connect = require('gulp-connect');
-var browserSync  = require('browser-sync').create();
 
-gulp.task('myStyles', function () {
-  gulp.src('./source/Sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./source/css'))
+gulp.task('sass', function () {
+  gulp.src('source/Sass/*.scss')
+    .pipe(scssPlugin())
+    .pipe(gulp.dest('css'))
     .pipe(connect.reload());
 });
 
@@ -20,78 +15,8 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('watchMyStyles', function() {
-  gulp.watch('./source/Sass/**/*.scss', ['myStyles']);
+gulp.task('watch', function() {
+  gulp.watch('source/Sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['watchMyStyles', 'connect']);
-
-var lr = require('tiny-lr'),
-  refresh = require('gulp-livereload'),
-  server = lr();
-
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./HTML5-JQuery-landing-demo/"
-    },
-    port: 3000
-  });
-  done();
-}
-
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
-
-// Clean assets
-function clean() {
-  return del(["./HTML5-JQuery-landing-demo/"]);
-}
-
-// Task SASS
-gulp.task('sass', function() {
-  gulp.src([
-    './source/Sass/**/*.scss',
-    '!sass/**/_*.scss'
-  ])
-    .pipe(sass({
-      includePaths: ['./source/Sass/style.scss']
-    }))
-    .pipe(gulp.dest('./source/css'))
-    .pipe(refresh(server));
-});
-
-
-gulp.task('refresh', function() {
-  gulp.src([
-    './*.html'
-  ])
-    .pipe(refresh(server));
-});
-
-
-// Task: default
-gulp.task('default', function() {
-  gulp.run('sass');
-
-  server.listen(35729, function (error) {
-    if (error) return console.log(error);
-
-    gulp.watch([
-      './source/Sass/**',
-      './source/img/**'
-    ], function(event) {
-      gulp.run('sass');
-    });
-
-    gulp.watch([
-      './*.html'
-    ], function(event) {
-      gulp.run('refresh');
-    });
-  });
-});
+gulp.task('default', ['watch', 'connect']);
