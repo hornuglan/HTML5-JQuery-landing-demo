8bc6d5a73f480344521fc3a6083ab19b9e0faae7
hornuglan <hornuglan@gmail.com>
2018-10-19 16:54:08 +0300

    the test version of listing at .projects block was added

---
+++ source/script/projects-listing.js
@@ -0,0 +1,15 @@
+$(".projects__button-link").click(function () {
+  $.ajax({
+    url: 'https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=yellow+flowers&image_type=photo&pretty=true',
+    success: function(msg){
+      console.log(msg.hits);
+      let arr = msg.hits;
+      let images = [];
+      for (let i = 0; i < arr.length; i++) {
+        images.push(arr[i].webformatURL);
+      }
+      const newDiv = `<div><img src="${images[0]}"></div>`;
+      $('.projects__wrapper').append(newDiv)
+    }
+  })});
+
