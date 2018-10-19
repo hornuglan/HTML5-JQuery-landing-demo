d82f4d10f5b03ace65296889403fb69c8001b6f0
hornuglan <hornuglan@gmail.com>
2018-10-19 20:13:21 +0300

    tags and basic styles were added to the listing items

--- source/script/projects-listing.js
+++ source/script/projects-listing.js
@@ -3,15 +3,12 @@ $(".projects__button-link").click(function () {
     url: 'https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=yellow+flowers&image_type=photo&pretty=true',
     success: function(msg){
       console.log(msg.hits);
-      let arr = msg.hits;
-      let images = [];
-      for (let i = 0; i < arr.length; i++) {
-        images.push(arr[i].webformatURL);
-      }
+      const images = msg.hits.map(function(item) {
+        return {url: item.webformatURL, tags: item.tags};
+      });
       for (let i = 0; i < images.length; i++) {
-        const newDiv = `<div><img src="${images[i]}"></div>`;
+        const newDiv = `<div class="projects__item col-sm-4 card-body"><img src="${images[i].url}"><p>${images[i].tags}</p></div>`;
         $('.projects__wrapper').append(newDiv)
       }
     }
   })});
-
