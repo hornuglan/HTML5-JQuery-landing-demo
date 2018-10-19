f096dd9dde8c3176c15b730b96f3dfd58fe51ceb
hornuglan <hornuglan@gmail.com>
2018-10-19 20:36:30 +0300

    styles for new items in the listing were added

--- source/script/projects-listing.js
+++ source/script/projects-listing.js
@@ -1,14 +1,19 @@
 $(".projects__button-link").click(function () {
   $.ajax({
     url: 'https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=yellow+flowers&image_type=photo&pretty=true',
-    success: function(msg){
+    success: function (msg) {
       console.log(msg.hits);
-      const images = msg.hits.map(function(item) {
+      const images = msg.hits.map(function (item) {
         return {url: item.webformatURL, tags: item.tags};
       });
       for (let i = 0; i < images.length; i++) {
-        const newDiv = `<div class="projects__item col-sm-4 card-body"><img src="${images[i].url}"><p>${images[i].tags}</p></div>`;
+        const newDiv = `<div class="projects__item col-sm-4 card-body">
+<img class="projects__item-img card-img-top" src="${images[i].url}">
+<h3 class="projects__item-title h3 card-title text-center">Claritas Etiam Processus</h3>
+<p class="projects__item-tag card-text text-center">${images[i].tags}</p>
+</div>`;
         $('.projects__wrapper').append(newDiv)
       }
     }
-  })});
+  })
+});
