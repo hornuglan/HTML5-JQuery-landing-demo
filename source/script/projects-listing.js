3e9de96ddb4b2d87038bb40a1eaa426dd358e808
hornuglan <hornuglan@gmail.com>
2018-10-21 12:58:41 +0300

    pictures are loading by .load() and by .click()

--- source/script/projects-listing.js
+++ source/script/projects-listing.js
@@ -1,21 +1,29 @@
-pageNumber = 1;
+pageNumber = 0;
+
+$(window).load(function () {
+  loadImages();
+});
+
 $(".projects__button-link").click(function () {
+  loadImages();
+});
+
+function loadImages() {
   pageNumber += 1;
   $.ajax({
     url: `https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=yellow+flowers&image_type=photo&pretty=true&page=${pageNumber}&per_page=6`,
     success: function (msg) {
-      console.log(msg.hits);
       const images = msg.hits.map(function (item) {
         return {url: item.webformatURL, tags: item.tags, title: item.user};
       });
-      for (let i = 0; i < images.length; i++) {
+      images.forEach(function (item) {
         const newDiv = `<div class="projects__item col-sm-4 card-body">
-<img class="projects__item-img card-img-top" src="${images[i].url}">
-<h3 class="projects__item-title h3 card-title text-center">${images[i].title}</h3>
-<p class="projects__item-tag card-text text-center">${images[i].tags}</p>
+<img class="projects__item-img card-img-top" src="${item.url}">
+<h3 class="projects__item-title h3 card-title text-center">${item.title}</h3>
+<p class="projects__item-tag card-text text-center">${item.tags}</p>
 </div>`;
         $('.projects__wrapper').append(newDiv)
-      }
+      })
     }
   })
-});
+}
