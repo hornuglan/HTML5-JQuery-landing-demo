12b5a29cdd06b18d2f6f8cfb869b6a08df8823bf
hornuglan <hornuglan@gmail.com>
2018-10-22 17:03:55 +0300

    all categories of images are loading after click on .category-item button All

--- source/script/projects-listing.js
+++ source/script/projects-listing.js
@@ -11,17 +11,44 @@ $(".projects__button-link").click(function () {
 
 $(".category__item").click(function () {
   pageNumber = 0;
-  categoryButton = $(this).context.innerText;
+  const category = $(this).context.innerHTML;
+  if (category === "All") {
+    categoryButton = null;
+  }
+  else {
+    categoryButton = category;
+  }
 
   $('.projects__wrapper').empty();
 
-  loadImages();
+  if (categoryButton) {
+    loadImages();
+  }
+  else {
+    pageNumber += 1;
+    $.ajax({
+      url: `https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&&image_type=photo&page=${pageNumber}&per_page=6`,
+      success: function (msg) {
+        const images = msg.hits.map(function (item) {
+          return {url: item.webformatURL, tags: item.tags, title: item.user};
+        });
+        images.forEach(function (item) {
+          const newDiv = `<div class="projects__item col-sm-4 card-body">
+<img class="projects__item-img card-img-top" src="${item.url}">
+<h3 class="projects__item-title h3 card-title text-center">${item.title}</h3>
+<p class="projects__item-tag card-text text-center">${item.tags}</p>
+</div>`;
+          $('.projects__wrapper').append(newDiv)
+        })
+      }
+    })
+  }
 });
 
 function loadImages() {
   pageNumber += 1;
   $.ajax({
-    url: `https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=${categoryButton}&image_type=photo&pretty=true&page=${pageNumber}&per_page=6`,
+    url: `https://pixabay.com/api/?key=10435611-b9a609b9561aa23df5e533662&q=${categoryButton}&image_type=photo&page=${pageNumber}&per_page=6`,
     success: function (msg) {
       const images = msg.hits.map(function (item) {
         return {url: item.webformatURL, tags: item.tags, title: item.user};
