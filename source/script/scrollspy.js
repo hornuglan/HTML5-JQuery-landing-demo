ca0bb83ae29d518cbb7431aa8c3a688f9da3d2fa
hornuglan <hornuglan@gmail.com>
2018-10-23 16:06:36 +0300

    test version of fixed header while scrolling was added

--- source/script/scrollspy.js
+++ source/script/scrollspy.js
@@ -1,1 +1,16 @@
-$('body').scrollspy({target: '.nav'});
+function scrollSpy() {
+  $('body').scrollspy({target: '.nav'});
+}
+
+window.onscroll = function() {attachingHeader()};
+    header = document.getElementById("header");
+    sticky = header.offsetTop;
+
+    function attachingHeader() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
