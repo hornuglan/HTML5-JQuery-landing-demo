89377b2b80262bc4e447125757427c7dba772471
hornuglan <hornuglan@gmail.com>
2018-10-23 19:14:44 +0300

    test version of smooth scroll was added

--- source/script/scrollspy.js
+++ source/script/scrollspy.js
@@ -1,16 +1,31 @@
+header = document.getElementById("header");
+sticky = header.offsetTop;
+headerOffset = $("header").height();
+
 function scrollSpy() {
-  $('body').scrollspy({target: '.nav'});
+  $('body').scrollspy({
+    target: '.nav'
+  });
 }
 
-window.onscroll = function() {attachingHeader()};
-    header = document.getElementById("header");
-    sticky = header.offsetTop;
+window.onscroll = function () {
+  attachingHeader()
+};
 
-    function attachingHeader() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-    }
+function attachingHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+  $('.nav-link').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'linear');
+  });
+
 
