5e32f8c1082cff403c0afed346a132c3c2aaa7e1
hornuglan <hornuglan@gmail.com>
2018-11-07 17:09:02 +0300

    css minifier and autoprefixer were added #15

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp');
 var scssPlugin = require('gulp-sass');
 var connect = require('gulp-connect');
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
+
+gulp.task('style', function () {
+  gulp.src('source/Sass/style.scss')
+    .pipe(plumber())
+    .pipe(scssPlugin())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('source/css'))
+});
 
 gulp.task('sass', function () {
   gulp.src('source/Sass/**/*.scss')
@@ -19,4 +37,4 @@ gulp.task('watch', function() {
   gulp.watch('source/Sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['watch', 'connect']);
+gulp.task('default', ['style', 'watch', 'connect']);
